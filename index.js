37c50f4669efb6883a9e264ff9c3fe0963b7918e
Omkar Daswadkar <158741267+omkar4771@users.noreply.github.com>
2024-06-22 19:51:14 +0530

    index file

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.set("views", path.join(__dirname ,"views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname,"public")));
 
+
 main()
 .then(()=>{
     console.log("connection sucessfull");
