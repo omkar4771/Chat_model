318300c8f97ef641b5e8b766cf96d117af982130
omkar4771 <omkardaswadkar123@gmail.com>
2024-06-26 22:58:29 +0530

    add error handling middleware

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+const ExpressError = require("./ExpressError.js");
 
 app.set("views", path.join(__dirname ,"views"));
 app.set("view engine", "ejs");
@@ -41,12 +42,23 @@ app.get("/chats",async (req,res)=>{
     res.render("chats.ejs",{chats});
 });
 //new routes
+app.get("/chats/new",(req,res)=>{
+    throw new ExpressError(404,"page not found");
+    res.render("new.ejs");
+})
+
+//new routes
 app.get("/chats/:name", async(req,res)=>{
     let {name} = req.params;
     const chats = await Chat.find();
     res.render("new.ejs",{chats,name});
 });
 
+//error handling middleware
+app.use((err,req,res,next)=>{
+    let{ status = 500, message = "Some error occure"} = err;
+    res.status(status).send(message);
+})
 //server starts
 app.listen(8080,()=>{
     console.log("app is listening on port 8080");
